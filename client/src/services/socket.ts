50e2d2b18cc275a594da6e599b4f8ac2b1919f53
Alan Babu <alanbabu518@gmail.com>
2026-09-25 00:38:31 +0530

    fix: use production socket url

--- client/src/services/socket.ts
+++ client/src/services/socket.ts
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:5000", {
+const SOCKET_URL = import.meta.env.VITE_API_BASE_URL?.replace("/api", "");
+
+export const socket = io(SOCKET_URL, {
   withCredentials: true,
 });
 
